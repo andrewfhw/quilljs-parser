88da77d45c09455b9a27a252383590bb82a764c7
Andrew Gilbert <andrew@scaffld.com>
2020-12-27 10:34:47 -0600

    basic tool setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function testing() {
+    return "success";
+}
---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+    mode: 'production',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'QuillParser',
+        libraryTarget: 'umd'
+    },
+    externals: {}
+};
+
+export default config;
